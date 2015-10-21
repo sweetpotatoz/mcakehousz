a967c1951f18d0b12d3cc2e5fd8d69df3747f4c1
sweetpotatoz <jitleong@gmail.com>
2015-10-21 17:59:36 +0800

    edit homepage

--- routes/gallery.js
+++ routes/gallery.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Gallery Page */
 router.get('/', function(req, res, next) {
-  res.render('gallery', { title: 'MCakeHousz' });
+  res.render('gallery', { title: 'MCakeHousZ' });
   /*res.send('respond with a resource');*/
 });
 
